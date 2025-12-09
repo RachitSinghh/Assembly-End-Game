9b8628359ffbed33e4ff2600c9ef0dc2a5dc5418
Rachit <rachitsingh72@gmail.com>
2025-12-09 15:57:25 +0530

    change in the plan

--- projectPlanning.md
+++ projectPlanning.md
@@ -10,7 +10,7 @@
 2. What values will need to be saved in state vs. what values can be derived from the state?
   - language container will be put in to the container. 
   - Input container where the state will be dervied from 
-  - keyboard container where it is changing it color
+  - keyboard container it is changing it color
   
 3. How will the user interact with the app? What events do I need to handle?
   - Keyboard Input and the the event that lets you interact with textinput in real time. 
--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,75 @@
-import React from 'react';
-import Header from './components/Header';
-import GameStatus from './components/GameStatus';
-import Languages from './components/Languages';
-import CurrentWord from './components/CurrentWord';
-import AlphabetEl from './components/AlphabetEl';
-import Button from './components/Button';
+import { useState } from "react";
+import { clsx } from "clsx";
+import { languages } from "./languages";
 
-function App() {
+
+export default function AssemblyEndgame() {
+  const [currentWord, setCurrentWord] = useState("react");
+  const [guessedLetters, setGuessedLetters] = useState([]);
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  function addGuessedLetter(letter) {
+    setGuessedLetters((prevLetters) =>
+      prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter],
+    );
+  }
+
+  const languageElements = languages.map((lang) => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    };
+    return (
+      <span className="chip" style={styles} key={lang.name}>
+        {lang.name}
+      </span>
+    );
+  });
+
+  const letterElements = currentWord
+    .split("")
+    .map((letter, index) => <span key={index}>{letter.toUpperCase()}</span>);
+
+  const keyboardElements = alphabet.split("").map((letter) => {
+    const isGuessed = guessedLetters.includes(letter);
+    const isCorrect = isGuessed && currentWord.includes(letter);
+    const isWrong = isGuessed && !currentWord.includes(letter);
+    const className = clsx({
+      correct: isCorrect,
+      wrong: isWrong,
+    });
+
+    console.log(className);
+
+    return (
+      <button
+        className={className}
+        key={letter}
+        onClick={() => addGuessedLetter(letter)}
+      >
+        {letter.toUpperCase()}
+      </button>
+    );
+  });
+
   return (
-   <main>
-     <Header />
-     <GameStatus />
-     <Languages />
-     <CurrentWord />
-     <AlphabetEl />
-     <Button />
-   </main>
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+      <section className="game-status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+      <section className="language-chips">{languageElements}</section>
+      <section className="word">{letterElements}</section>
+      <section className="keyboard">{keyboardElements}</section>
+      <button className="new-game">New Game</button>
+    </main>
   );
 }
-
-export default App;
